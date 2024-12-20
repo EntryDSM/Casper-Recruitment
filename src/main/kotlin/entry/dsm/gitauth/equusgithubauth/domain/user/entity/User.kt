3ac718b5d58a90c0f6acea99612efe7160d36cb7
강은찬 <kangeunchan080310@gmail.com>
2024-12-20 09:02:56 +0900

    feat : (#2) Remove builder annotation in user data class

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/entity/User.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/entity/User.kt
@@ -4,11 +4,9 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import lombok.Builder
 import java.time.LocalDateTime
 import java.util.*
 
-@Builder
 @Entity
 @Table(name = "users")
 data class User(
