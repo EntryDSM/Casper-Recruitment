fe5fa8e8a66caa6aca877efaa525a317ffdbd26b
강은찬 <kangeunchan080310@gmail.com>
2024-12-20 09:16:47 +0900

    feat : (#2) Change GithubUserTokenValidationService use TokenAuthenticator

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/GithubUserTokenValidationService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/GithubUserTokenValidationService.kt
@@ -1,16 +1,18 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.service
 
 import entry.dsm.gitauth.equusgithubauth.domain.auth.client.GithubApiClient
+import entry.dsm.gitauth.equusgithubauth.global.external.service.TokenAuthenticator
 import org.springframework.stereotype.Service
 
 @Service
 class GithubUserTokenValidationService(
-    private val githubClient: GithubApiClient
+    private val githubClient: GithubApiClient,
+    private val tokenAuthenticator: TokenAuthenticator
 ) {
     fun validateAccessToken(token: String) {
         require(token.isNotBlank()) { "Access token is empty." }
         try {
-            githubClient.getUser("Bearer $token")
+            githubClient.getUser(tokenAuthenticator.createAuthorizationHeader(token))
         } catch (ex: Exception) {
             throw IllegalArgumentException("Access token is expired or invalid.")
         }
