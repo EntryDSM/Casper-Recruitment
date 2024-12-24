23458c864f55a307d2112cad48ea2cede9c37d96
도훈 <modooboiroo@gmail.com>
2024-12-24 21:18:36 +0900

    feat : (#5) add custom getters for statusCode and message based on errorCode

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/error/exception/BusinessException.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/error/exception/BusinessException.kt
@@ -3,4 +3,10 @@ package entry.dsm.gitauth.equusgithubauth.global.error.exception
 
 abstract class BusinessException(
     val errorCode: ErrorCode
-): RuntimeException()
+): RuntimeException() {
+    val statusCode: Int
+        get() = errorCode.statusCode
+
+    override val message: String
+        get() = errorCode.message
+}
