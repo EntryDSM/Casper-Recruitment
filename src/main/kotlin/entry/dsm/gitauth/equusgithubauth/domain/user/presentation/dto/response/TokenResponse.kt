6736cd616f426e1bad430aa1a156de94e3bbe928
도훈 <modooboiroo@gmail.com>
2024-12-24 21:44:41 +0900

    feat : (#5) add TokenResponse

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/presentation/dto/response/TokenResponse.kt
@@ -0,0 +1,10 @@
+package entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response
+
+import java.time.LocalDateTime
+
+data class TokenResponse(
+    val accessToken: String,
+    val accessTokenExpiration: LocalDateTime,
+    val refreshToken: String,
+    val refreshTokenExpiration: LocalDateTime
+)
