131ce6917c24587a23207de389456ff73c1eddd8
도훈 <modooboiroo@gmail.com>
2024-12-24 20:54:16 +0900

    feat : (#5) add @EnableConfigurationProperties annotation to EquusGithubAuthApplication.kt

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/EquusGithubAuthApplication.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/EquusGithubAuthApplication.kt
@@ -1,11 +1,14 @@
 package entry.dsm.gitauth.equusgithubauth
 
+import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableConfigurationProperties(value = [ JwtProperties::class])
 class EquusGithubAuthApplication
 
 fun main(args: Array<String>) {
