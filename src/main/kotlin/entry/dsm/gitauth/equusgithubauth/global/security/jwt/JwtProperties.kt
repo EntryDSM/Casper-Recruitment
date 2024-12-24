448e12b812c1963c4339d58078a6ea4fe94f946a
coehgns <modooboiroo@gmail.com>
2024-12-24 20:55:54 +0900

    feat : (#5) add JwtProperties

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/security/jwt/JwtProperties.kt
@@ -0,0 +1,12 @@
+package entry.dsm.gitauth.equusgithubauth.global.security.jwt
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "jwt")
+class JwtProperties(
+    val header: String,
+    val prefix: String,
+    val secretKey: String,
+    val accessExp: Long,
+    val refreshExp: Long
+)
