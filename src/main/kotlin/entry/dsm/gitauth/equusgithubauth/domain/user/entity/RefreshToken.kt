ef731301d71662808d94188a8c6bc7a79c644d71
도훈 <modooboiroo@gmail.com>
2024-12-24 22:03:54 +0900

    refactor : (#5) remove email, username field to RefreshToken

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/entity/RefreshToken.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/entity/RefreshToken.kt
@@ -12,12 +12,6 @@ data class RefreshToken(
     @Column(name = "id", nullable = false, unique = true)
     val githubId: String,
 
-    @Column(name = "username", nullable = false)
-    val username: String,
-
-    @Column(name = "email", nullable = true, unique = false)
-    val email: String,
-
     @Column(name = "refresh_token")
     var refreshToken: String? = null,
 
