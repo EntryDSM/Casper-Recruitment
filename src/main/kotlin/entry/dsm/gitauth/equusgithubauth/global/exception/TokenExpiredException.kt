a31a5991decb5c4432778370d1bf3ad952c911a3
도훈 <modooboiroo@gmail.com>
2024-12-24 22:28:47 +0900

    feat : (#5) add TokenExpiredException

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/exception/TokenExpiredException.kt
@@ -0,0 +1,7 @@
+package entry.dsm.gitauth.equusgithubauth.global.exception
+
+import entry.dsm.gitauth.equusgithubauth.global.error.exception.BusinessException
+import entry.dsm.gitauth.equusgithubauth.global.error.exception.ErrorCode
+
+object TokenExpiredException: BusinessException(ErrorCode.USER_NOT_FOUND) {
+}
