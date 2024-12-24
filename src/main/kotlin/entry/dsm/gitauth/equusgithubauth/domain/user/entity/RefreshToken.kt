c018cc495004b80ab7304b95db5a3c175b0abd13
도훈 <modooboiroo@gmail.com>
2024-12-24 22:06:10 +0900

    feat : (#5) add redis settings to RefreshToken

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/entity/RefreshToken.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/entity/RefreshToken.kt
@@ -4,7 +4,11 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
 
+@RedisHash
 @Entity
 @Table(name = "refresh_token")
 data class RefreshToken(
@@ -12,9 +16,11 @@ data class RefreshToken(
     @Column(name = "id", nullable = false, unique = true)
     val githubId: String,
 
+    @Indexed
     @Column(name = "refresh_token")
     var refreshToken: String? = null,
 
+    @TimeToLive
     @Column(name = "token_expiration")
     var tokenExpiration: Long? = null
 )
