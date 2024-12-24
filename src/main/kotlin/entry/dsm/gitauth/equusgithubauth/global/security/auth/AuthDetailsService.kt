a0622c7dffe49dcb019c9f75e98407f7bf31064e
도훈 <modooboiroo@gmail.com>
2024-12-24 21:28:16 +0900

    feat : (#5) add AuthDetailsService

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/security/auth/AuthDetailsService.kt
@@ -0,0 +1,21 @@
+package entry.dsm.gitauth.equusgithubauth.global.security.auth
+
+import entry.dsm.gitauth.equusgithubauth.domain.user.entity.User
+import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
+import entry.dsm.gitauth.equusgithubauth.domain.user.exception.UserNotFoundException
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+
+@Service
+class AuthDetailsService(
+    private val userRepository: UserRepository
+): UserDetailsService {
+
+    override fun loadUserByUsername(githubId: String): UserDetails {
+        val user: User = userRepository.findByGithubId(githubId)
+            ?: throw UserNotFoundException
+
+        return AuthDetails(user)
+    }
+}
