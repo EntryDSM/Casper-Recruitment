a6e0bdbb06685a9b7456982aad1d33d4f74bd56a
강은찬 <kangeunchan080310@gmail.com>
2024-12-29 00:36:04 +0900

    feat : (#5) add token generation and return logic in UserService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
@@ -3,15 +3,18 @@ package entry.dsm.gitauth.equusgithubauth.domain.user.service
 import entry.dsm.gitauth.equusgithubauth.domain.auth.service.GithubUserService
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.User
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
+import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response.TokenResponse
+import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
     private val userRepository: UserRepository,
-    private val GithubUserService: GithubUserService
+    private val GithubUserService: GithubUserService,
+    private val jwtTokenProvider: JwtTokenProvider
 ) {
-    fun  execute(oAuth2User: OAuth2User) : User {
+    fun  execute(oAuth2User: OAuth2User) : TokenResponse {
         val userInfo = GithubUserService.getGithubUserInformation(oAuth2User)
 
         val user = User(
@@ -26,12 +29,12 @@ class UserService(
             tokenExpiration = userInfo.tokenExpiration
         )
 
+        val token = jwtTokenProvider.generateToken(userInfo.githubId)
+
         if (userRepository.findByGithubId(userInfo.githubId) != null) {
-            return userRepository.findByGithubId(userInfo.githubId)!!
+            return token
         }
-
-        // TODO : Token을 생성하고 반환하는 로직
-
-        return userRepository.save(user) // TODO : 토큰을 반환하게
+        userRepository.save(user)
+        return token
     }
 }
