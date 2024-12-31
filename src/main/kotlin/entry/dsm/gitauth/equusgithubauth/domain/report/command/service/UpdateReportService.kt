e56e8fc106d817e58a2de19f4952ecd6ac3bb2a0
박주원 <qkrwndnjs7308@gmail.com>
2024-12-31 20:02:09 +0900

    feat : (#9) add UpdateReportService

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/report/command/service/UpdateReportService.kt
@@ -0,0 +1,34 @@
+package entry.dsm.gitauth.equusgithubauth.domain.report.command.service
+
+import entry.dsm.gitauth.equusgithubauth.domain.report.command.dto.request.UpdateReportCommand
+import entry.dsm.gitauth.equusgithubauth.domain.report.command.repository.ReportRepository
+import entry.dsm.gitauth.equusgithubauth.domain.report.entity.Report
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UpdateReportService(
+    private val reportRepository: ReportRepository
+) {
+
+    @Transactional
+    fun updateReport(reportId: Long, command: UpdateReportCommand): Report {
+
+        val report = reportRepository.findByIdOrNull(reportId)
+            ?: throw IllegalArgumentException("Report with id $reportId not found")
+
+
+        report.apply {
+            applicantName = command.applicantName
+            studentId = command.studentId
+            phoneNumber = command.phoneNumber
+            programmingExperience = command.programmingExperience
+            major = command.major
+            motivation = command.motivation
+            selfIntroduction = command.selfIntroduction
+        }
+
+        return reportRepository.save(report)
+    }
+}
