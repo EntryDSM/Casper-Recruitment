de543ffa4b5c1d26733668a4f15b233b99b48103
박주원 <qkrwndnjs7308@gmail.com>
2024-12-31 20:03:45 +0900

    feat : (#9) add ReportQueryService

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/report/query/service/ReportQueryService.kt
@@ -0,0 +1,20 @@
+package entry.dsm.gitauth.equusgithubauth.domain.report.query.service
+
+import entry.dsm.gitauth.equusgithubauth.domain.report.query.dto.response.ReportQueryResponse
+import entry.dsm.gitauth.equusgithubauth.domain.report.query.repository.ReportQueryRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class ReportQueryService(
+    private val reportQueryRepository: ReportQueryRepository
+) {
+
+    @Transactional(readOnly = true)
+    fun getReport(reportId: Long): ReportQueryResponse {
+        val report = reportQueryRepository.findByIdOrNull(reportId)
+            ?: throw IllegalArgumentException("Report with id $reportId not found")
+        return ReportQueryResponse.from(report)
+    }
+}
