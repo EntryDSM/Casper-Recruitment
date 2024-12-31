9c52b69025d0cbef89e7c5da0ac5b9d75e8d3ac5
박주원 <qkrwndnjs7308@gmail.com>
2024-12-31 20:01:23 +0900

    feat : (#9) add CreateReportCommand Dto

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/report/command/dto/request/CreateReportCommand.kt
@@ -0,0 +1,15 @@
+package entry.dsm.gitauth.equusgithubauth.domain.report.command.dto.request
+
+import entry.dsm.gitauth.equusgithubauth.domain.report.entity.enums.Major
+import entry.dsm.gitauth.equusgithubauth.domain.report.entity.enums.ProgrammingExperience
+
+data class CreateReportCommand(
+    val noticeId: Long,
+    val applicantName: String,
+    val studentId: String,
+    val phoneNumber: String,
+    val programmingExperience: ProgrammingExperience,
+    val major: Major,
+    val motivation: String,
+    val selfIntroduction: String
+)
