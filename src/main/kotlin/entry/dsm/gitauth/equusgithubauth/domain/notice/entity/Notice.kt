18be60bf4338ac0f37dd836ee71ff1737268cd5f
박주원 <qkrwndnjs7308@gmail.com>
2024-12-31 17:54:30 +0900

    feat : (#6) add Notice Entity

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/entity/Notice.kt
@@ -0,0 +1,46 @@
+package entry.dsm.gitauth.equusgithubauth.domain.notice.entity
+
+import entry.dsm.gitauth.equusgithubauth.domain.notice.command.dto.request.UpdateNoticeCommand
+import entry.dsm.gitauth.equusgithubauth.domain.notice.entity.enums.ImportanceOfRecruit
+import entry.dsm.gitauth.equusgithubauth.domain.report.entity.Report
+import jakarta.persistence.*
+import jakarta.persistence.OneToMany;
+
+@Entity(name = "application")
+class Notice(
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    val noticeId: Long,
+
+    @Column(name = "title", nullable = false)
+    var title: String,
+
+    @Column(name = "key_word", nullable = false)
+    var keyWord: String,
+
+    @Column(name = "title_image_url", nullable = false)
+    var titleImageUrl: String,
+
+    @Column(name = "description", nullable = false)
+    var description: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "importance_of_recruit", nullable = false)
+    var importanceOfRecruit: ImportanceOfRecruit,
+
+    @OneToMany(mappedBy = "notice", fetch = FetchType.LAZY)
+    val reports: List<Report> = listOf()
+
+
+) {
+    fun update(command: UpdateNoticeCommand) {
+        title = command.title
+        keyWord = command.keyWord
+        titleImageUrl = command.titleImageUrl
+        description = command.description
+        importanceOfRecruit = command.importanceOfRecruit
+    }
+
+}
