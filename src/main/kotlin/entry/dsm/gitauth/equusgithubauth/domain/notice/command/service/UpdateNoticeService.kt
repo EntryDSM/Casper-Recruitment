8139e658420b11368e21347c610fcf81c4a6ab8b
박주원 <qkrwndnjs7308@gmail.com>
2024-12-31 18:00:44 +0900

    feat : (#6) UpdateNoticeService

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/command/service/UpdateNoticeService.kt
@@ -0,0 +1,22 @@
+package entry.dsm.gitauth.equusgithubauth.domain.notice.command.service
+
+import entry.dsm.gitauth.equusgithubauth.domain.notice.command.repository.NoticeRepository
+import entry.dsm.gitauth.equusgithubauth.domain.notice.command.dto.request.UpdateNoticeCommand
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UpdateNoticeService(
+    private val noticeRepository: NoticeRepository
+) {
+
+    @Transactional
+    fun updateNotice(noticeId: Long, command: UpdateNoticeCommand) {
+        val notice = noticeRepository.findByIdOrNull(noticeId)
+            ?: throw IllegalArgumentException("Notice with id $noticeId not found")
+
+        notice.update(command)
+
+    }
+}
