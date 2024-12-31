07d5ac2a18a3e5efb5c5d6906ea83bbc13035fba
박주원 <qkrwndnjs7308@gmail.com>
2024-12-31 18:00:13 +0900

    feat : (#6) add CreateNoticeService

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/command/service/CreateNoticeService.kt
@@ -0,0 +1,27 @@
+package entry.dsm.gitauth.equusgithubauth.domain.notice.command.service
+
+import entry.dsm.gitauth.equusgithubauth.domain.notice.entity.Notice
+import entry.dsm.gitauth.equusgithubauth.domain.notice.command.repository.NoticeRepository
+import entry.dsm.gitauth.equusgithubauth.domain.notice.command.dto.request.CreateNoticeCommand
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class CreateNoticeService(
+    private val noticeRepository: NoticeRepository
+) {
+
+    @Transactional
+    fun createNotice(command: CreateNoticeCommand) {
+        val notice = Notice(
+            noticeId = command.noticeId,
+            title = command.title,
+            keyWord = command.keyWord,
+            titleImageUrl = command.titleImageUrl,
+            description = command.description,
+            importanceOfRecruit = command.importanceOfRecruit,
+            reports = listOf()
+        )
+        noticeRepository.save(notice)
+    }
+}
