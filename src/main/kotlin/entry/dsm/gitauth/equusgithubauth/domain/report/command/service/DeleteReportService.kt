c9f30cd1022108325a72bc53268ef85c3d36604f
박주원 <qkrwndnjs7308@gmail.com>
2024-12-31 20:02:18 +0900

    feat : (#9) add DeleteReportService

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/report/command/service/DeleteReportService.kt
@@ -0,0 +1,20 @@
+package entry.dsm.gitauth.equusgithubauth.domain.report.command.service
+
+import entry.dsm.gitauth.equusgithubauth.domain.report.command.repository.ReportRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class DeleteReportService(
+    private val reportRepository: ReportRepository
+) {
+
+    @Transactional
+    fun deleteReport(reportId: Long) {
+        val report = reportRepository.findByIdOrNull(reportId)
+            ?: throw IllegalArgumentException("Report with id $reportId not found")
+
+        reportRepository.delete(report)
+    }
+}
