e99cdf49da12a3e6ec77c5927e50437faf2491bc
강은찬 <kangeunchan080310@gmail.com>
2025-01-07 16:50:20 +0900

    fix : (#12) resolve merge conflicts in CreateNoticeCommand and UpdateNoticeCommand

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/command/dto/request/CreateNoticeCommand.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/command/dto/request/CreateNoticeCommand.kt
@@ -6,24 +6,19 @@ import jakarta.validation.constraints.Size
 
 data class CreateNoticeCommand(
     val noticeId: Long,
+
     @NotBlank(message = "제목은 비워둘 수 없습니다")
     @Size(min = 5, max = 100, message = "제목은 최소 5글자, 최대 100글자까지 가능합니다")
     val title: String,
+
     @Size(min = 2, max = 50, message = "키워드는 최소 2글자, 최대 50글자까지 가능합니다.")
     val keyWord: String,
+
     val titleImageUrl: String,
+
     @NotBlank(message = "설명은 비워둘 수 없습니다")
     val description: String,
-<<<<<<< HEAD
-    @NotBlank
-    val importanceOfRecruit: ImportanceOfRecruit,
-)
-=======
+
+    @NotBlank(message = "모집 중요도는 비워둘 수 없습니다")
     val importanceOfRecruit: ImportanceOfRecruit
 )
-<<<<<<< HEAD
-// conflict 이슈로 나중에 검증 추가 예정
->>>>>>> 0f16680 (docs : (#9))
-=======
-// conflict 이슈로 나중에 검증 추가 예정
->>>>>>> origin/feature/9-develop-application-resubmission-features
