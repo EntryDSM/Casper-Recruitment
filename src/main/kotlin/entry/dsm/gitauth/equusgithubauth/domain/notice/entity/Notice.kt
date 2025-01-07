8cac3ff0cb95b1800a892654120e0e8773e470a1
강은찬 <kangeunchan080310@gmail.com>
2025-01-07 17:29:18 +0900

    chore : (#13) apply ktlint formatting rules to Kotlin files

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/entity/Notice.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/entity/Notice.kt
@@ -3,7 +3,14 @@ package entry.dsm.gitauth.equusgithubauth.domain.notice.entity
 import entry.dsm.gitauth.equusgithubauth.domain.notice.command.dto.request.UpdateNoticeCommand
 import entry.dsm.gitauth.equusgithubauth.domain.notice.entity.enums.ImportanceOfRecruit
 import entry.dsm.gitauth.equusgithubauth.domain.report.entity.Report
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Column
+import jakarta.persistence.Enumerated
+import jakarta.persistence.EnumType
+import jakarta.persistence.FetchType
+import jakarta.persistence.OneToMany
 
 @Entity(name = "notice")
 class Notice(
