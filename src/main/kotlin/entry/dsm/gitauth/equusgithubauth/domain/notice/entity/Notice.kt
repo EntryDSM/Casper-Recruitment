3c80388d208dad51ade36088499cf77335c225a9
박주원 <qkrwndnjs7308@gmail.com>
2025-01-20 17:11:49 +0900

    refactor : (#22) Modify column type

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/entity/Notice.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/entity/Notice.kt
@@ -3,13 +3,16 @@ package entry.dsm.gitauth.equusgithubauth.domain.notice.entity
 import entry.dsm.gitauth.equusgithubauth.domain.notice.command.dto.request.UpdateNoticeCommand
 import entry.dsm.gitauth.equusgithubauth.domain.notice.entity.enums.ImportanceOfRecruit
 import entry.dsm.gitauth.equusgithubauth.domain.report.entity.Report
+import jakarta.persistence.CollectionTable
 import jakarta.persistence.Column
+import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToMany
 
 @Entity(name = "notice")
@@ -20,12 +23,16 @@ class Notice(
     val noticeId: Long,
     @Column(name = "title", nullable = false)
     var title: String,
+    @ElementCollection
+    @CollectionTable(name = "notice_keywords", joinColumns = [JoinColumn(name = "notice_id")])
     @Column(name = "key_word", nullable = false)
-    var keyWord: String,
+    var keyWord: List<String>,
     @Column(name = "title_image_url", nullable = false)
     var titleImageUrl: String,
+    @ElementCollection
+    @CollectionTable(name = "notice_descriptions", joinColumns = [JoinColumn(name = "notice_id")])
     @Column(name = "description", nullable = false)
-    var description: String,
+    var description: List<String>,
     @Enumerated(EnumType.STRING)
     @Column(name = "importance_of_recruit", nullable = false)
     var importanceOfRecruit: ImportanceOfRecruit,
