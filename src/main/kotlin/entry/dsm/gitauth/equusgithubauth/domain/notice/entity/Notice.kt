488e1e88fcd598a9d5501eb6a1bea560d318a21f
kangeunchan <kangeunchan080310@gmail.com>
2025-02-12 15:29:22 +0900

    fix : (#15) ktlintFormat

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/entity/Notice.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/entity/Notice.kt
@@ -2,7 +2,18 @@ package entry.dsm.gitauth.equusgithubauth.domain.notice.entity
 
 import entry.dsm.gitauth.equusgithubauth.domain.notice.command.dto.request.UpdateNoticeCommand
 import entry.dsm.gitauth.equusgithubauth.domain.report.entity.Report
-import jakarta.persistence.*
+import jakarta.persistence.CascadeType
+import jakarta.persistence.CollectionTable
+import jakarta.persistence.Column
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToMany
 
 @Entity(name = "notice")
 class Notice(
--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/entity/NoticeDescription.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/entity/NoticeDescription.kt
@@ -1,6 +1,12 @@
 package entry.dsm.gitauth.equusgithubauth.domain.notice.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 
 @Entity(name = "notice_description")
 class NoticeDescription(
