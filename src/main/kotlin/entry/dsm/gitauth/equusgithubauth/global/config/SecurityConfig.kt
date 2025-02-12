f5ea1e058030f0541f564c5a373533b71f049be9
도훈 <modooboiroo@gmail.com>
2025-02-12 22:28:14 +0900

    fix : (#29) cors setting

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/config/SecurityConfig.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/config/SecurityConfig.kt
@@ -43,8 +43,8 @@ class SecurityConfig(
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("*")
-        configuration.allowedMethods = listOf("*")
+        configuration.allowedOrigins = listOf("http://localhost:5173")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
 
