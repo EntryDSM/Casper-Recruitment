aac39b3c921b772da4f8476eb7e781dfd99c01e8
coehgns <modooboiroo@gmail.com>
2025-03-10 16:50:33 +0900

    chore : (#32) remove unnecessary files

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/exception/EmptyAccessTokenException.kt
+++
@@ -1,6 +0,0 @@
-package entry.dsm.gitauth.equusgithubauth.domain.auth.exception
-
-import entry.dsm.gitauth.equusgithubauth.global.exception.CustomException
-import entry.dsm.gitauth.equusgithubauth.global.exception.ErrorCode
-
-object EmptyAccessTokenException : CustomException(ErrorCode.EMPTY_ACCESS_TOKEN)
