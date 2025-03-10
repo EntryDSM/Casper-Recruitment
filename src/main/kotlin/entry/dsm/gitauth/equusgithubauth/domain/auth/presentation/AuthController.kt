f2c1f107d68cb0ceefd1aec38b3af42a0ebefeab
coehgns <modooboiroo@gmail.com>
2025-03-10 11:02:58 +0900

    feat : (#32) add login method to AuthController

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/AuthController.kt
@@ -1,8 +1,10 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.presentation
 
+import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request.LoginRequest
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request.SignUpRequest
 import entry.dsm.gitauth.equusgithubauth.domain.auth.service.SignUpService
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.TokenResponse
+import entry.dsm.gitauth.equusgithubauth.domain.auth.service.LoginService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,12 +12,20 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/auth")
 class AuthController(
-    private val signUpService: SignUpService
+    private val signUpService: SignUpService,
+    private val loginService: LoginService
 ) {
     @PostMapping("/sign-up")
     fun signUp(
         request: SignUpRequest
     ): TokenResponse {
         return signUpService.execute(request)
+    }
+
+    @PostMapping("login")
+    fun login(
+        request: LoginRequest
+    ): TokenResponse {
+        return loginService.execute(request)
     }
 }
