a6668388b2c159cefa0047e3938b593c2465d909
coehgns <modooboiroo@gmail.com>
2025-03-10 16:59:24 +0900

    feat : (#32) add valid setting to SignUpRequest

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/dto/request/SignUpRequest.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/dto/request/SignUpRequest.kt
@@ -3,8 +3,10 @@ package entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request
 import entry.dsm.gitauth.equusgithubauth.global.util.RegexpUtil
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
+import org.hibernate.validator.constraints.Length
 
 data class SignUpRequest(
+    @field:Length(min = 1, max = 15)
     @field:NotBlank
     val username: String,
 
