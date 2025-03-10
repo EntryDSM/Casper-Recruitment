16aa3b38d95b9dc0da907018c755befcefee6973
coehgns <modooboiroo@gmail.com>
2025-03-10 10:50:00 +0900

    feat: (#32) SignUpService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/SignUpService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/SignUpService.kt
@@ -1,4 +1,35 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.service
 
-class SignUpService {
+import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request.SignUpRequest
+import entry.dsm.gitauth.equusgithubauth.domain.user.entity.User
+import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
+import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response.TokenResponse
+import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+
+@Service
+class SignUpService(
+    private val passwordEncoder: PasswordEncoder,
+    private val userRepository: UserRepository,
+    private val jwtTokenProvider: JwtTokenProvider
+) {
+    @Transactional
+    fun execute(
+        request: SignUpRequest
+    ): TokenResponse {
+        val user = request.run {
+            User(
+                username = username,
+                password = passwordEncoder.encode(password),
+                createdAt = LocalDateTime.now(),
+                updatedAt = LocalDateTime.now(),
+            )
+        }
+        userRepository.save(user)
+
+        return jwtTokenProvider.generateToken(user.username)
+    }
 }
