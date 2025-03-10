65f05b2741217306a4adab67b9ac571209768da2
coehgns <modooboiroo@gmail.com>
2025-03-10 17:21:57 +0900

    chore : (#32) remove unnecessary value to ErrorCode

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/exception/ErrorCode.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/exception/ErrorCode.kt
@@ -5,12 +5,6 @@ enum class ErrorCode(
     val message: String,
 ) {
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
-    USER_INFO_FETCH_ERROR(500, "GitHub 사용자 정보를 가져올 수 없습니다."),
-    AUTHORIZED_CLIENT_NOT_FOUND(400, "사용자에 대한 인증된 클라이언트를 찾을 수 없습니다."),
-    TOKEN_EXPIRATION_MISSING(400, "토큰 만료 시간이 누락되었습니다."),
-    INVALID_ACCESS_TOKEN(400, "액세스 토큰이 만료되었거나 유효하지 않습니다."),
-    EMPTY_ACCESS_TOKEN(400, "액세스 토큰이 비어 있습니다."),
-
     NOTICE_NOT_FOUND(404, "공지사항을 찾을 수 없습니다."),
 
     REPORT_NOT_FOUND(404, "보고서를 찾을 수 없습니다."),
