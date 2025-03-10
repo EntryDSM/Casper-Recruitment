498217f0c59fdf87095464c29c47271b18cda1ca
coehgns <modooboiroo@gmail.com>
2025-03-10 16:57:50 +0900

    feat : (#32) add valid setting to SignUpRequest

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/dto/request/SignUpRequest.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/dto/request/SignUpRequest.kt
@@ -1,6 +1,14 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request
 
+import entry.dsm.gitauth.equusgithubauth.global.util.RegexpUtil
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+
 data class SignUpRequest(
+    @field:NotBlank
     val username: String,
+
+    @field:Pattern(regexp = RegexpUtil.PASSWORD_PATTERN)
+    @field:NotBlank
     val password: String
 )
