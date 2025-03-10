faf9be8350becdce045e32c3211b8c2ba961a90b
coehgns <modooboiroo@gmail.com>
2025-03-10 11:24:34 +0900

    feat : (#32) add userFacade login to LoginService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/LoginService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/LoginService.kt
@@ -3,7 +3,7 @@ package entry.dsm.gitauth.equusgithubauth.domain.auth.service
 import entry.dsm.gitauth.equusgithubauth.domain.auth.exception.PasswordMissMatchException
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request.LoginRequest
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.TokenResponse
-import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
+import entry.dsm.gitauth.equusgithubauth.domain.user.userFacade.UserFacade
 import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -11,15 +11,15 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class LoginService(
-    private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder,
-    private val jwtTokenProvider: JwtTokenProvider
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val userFacade: UserFacade
 ) {
     @Transactional
     fun execute(
         request: LoginRequest
     ): TokenResponse {
-        val user = userRepository.findByUsername(request.username)
+        val user = userFacade.findByUsername(request.username)
 
         if (passwordEncoder.matches(user.password, request.password)) {
             throw PasswordMissMatchException
