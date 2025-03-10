1864f92ee385f82c3ba288732d33bf5960792918
coehgns <modooboiroo@gmail.com>
2025-03-10 11:13:13 +0900

    feat : (#32) add tokenRefresh method to AuthController

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/AuthController.kt
@@ -2,9 +2,11 @@ package entry.dsm.gitauth.equusgithubauth.domain.auth.presentation
 
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request.LoginRequest
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request.SignUpRequest
+import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request.TokenRefreshRequest
 import entry.dsm.gitauth.equusgithubauth.domain.auth.service.SignUpService
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.TokenResponse
 import entry.dsm.gitauth.equusgithubauth.domain.auth.service.LoginService
+import entry.dsm.gitauth.equusgithubauth.domain.auth.service.TokenRefreshService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/auth")
 class AuthController(
     private val signUpService: SignUpService,
-    private val loginService: LoginService
+    private val loginService: LoginService,
+    private val tokenRefreshService: TokenRefreshService
 ) {
     @PostMapping("/sign-up")
     fun signUp(
@@ -27,5 +30,12 @@ class AuthController(
         request: LoginRequest
     ): TokenResponse {
         return loginService.execute(request)
+    }
+
+    @PostMapping("token-refresh")
+    fun tokenRefresh(
+        request: TokenRefreshRequest
+    ): TokenResponse {
+        return tokenRefreshService.execute(request)
     }
 }
