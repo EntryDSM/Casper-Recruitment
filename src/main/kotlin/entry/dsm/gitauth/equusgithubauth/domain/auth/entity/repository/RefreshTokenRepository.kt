6f12999a58fed6f7ed7ee3b97c961cf22fa17b59
coehgns <modooboiroo@gmail.com>
2025-03-10 11:12:25 +0900

    feat : (#32) add findByToken method to RefreshTokenRepository

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/entity/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/entity/repository/RefreshTokenRepository.kt
@@ -3,4 +3,6 @@ package entry.dsm.gitauth.equusgithubauth.domain.auth.entity.repository
 import entry.dsm.gitauth.equusgithubauth.domain.auth.entity.RefreshToken
 import org.springframework.data.repository.CrudRepository
 
-interface RefreshTokenRepository : CrudRepository<RefreshToken, String>
+interface RefreshTokenRepository : CrudRepository<RefreshToken, String> {
+    fun findByToken(token: String): RefreshToken
+}
