3fa986ecc87b1c31facab47c15865db7d0e68d76
coehgns <modooboiroo@gmail.com>
2025-03-10 16:59:29 +0900

    feat : (#32) add valid setting to LoginRequest

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/dto/request/LoginRequest.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/dto/request/LoginRequest.kt
@@ -1,6 +1,14 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request
 
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Length
+
 data class LoginRequest(
+    @field:Length(min = 1, max = 15)
+    @field:NotBlank
     val username: String,
+
+    @field:Length(min = 10, max = 30)
+    @field:NotBlank
     val password: String
 )
