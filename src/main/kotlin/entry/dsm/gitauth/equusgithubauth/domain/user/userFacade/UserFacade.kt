ea1ae8da75259819849f0692db7498a5021c14d5
coehgns <modooboiroo@gmail.com>
2025-03-10 11:24:09 +0900

    feat : (#32) UserFacade

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/userFacade/UserFacade.kt
@@ -0,0 +1,18 @@
+package entry.dsm.gitauth.equusgithubauth.domain.user.userFacade
+
+import entry.dsm.gitauth.equusgithubauth.domain.user.entity.User
+import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
+import entry.dsm.gitauth.equusgithubauth.global.security.auth.exception.UserNotFoundException
+import org.springframework.stereotype.Component
+
+@Component
+class UserFacade(
+    private val userRepository: UserRepository
+) {
+    fun findByUsername(
+        username: String
+    ): User {
+        return userRepository.findByUsername(username)
+            ?: throw UserNotFoundException
+    }
+}
