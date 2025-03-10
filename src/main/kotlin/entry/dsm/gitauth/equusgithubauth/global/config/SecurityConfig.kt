ed72a7c90c251aa5c5c93a81a6ef9c0e347f4408
coehgns <modooboiroo@gmail.com>
2025-03-10 16:00:46 +0900

    refactor : (#32) remove client related logic to SecurityConfig

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/config/SecurityConfig.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/config/SecurityConfig.kt
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -19,7 +18,6 @@ class SecurityConfig(
     @Bean
     fun securityFilterChain(
         http: HttpSecurity,
-        clientRegistrationRepository: ClientRegistrationRepository,
     ): SecurityFilterChain {
         http
             .csrf { it.disable() }
