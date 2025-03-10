75e6232a352224de1785f61dd05b298faf70b643
coehgns <modooboiroo@gmail.com>
2025-03-10 10:49:00 +0900

    feat: (#32) add bean(passwordEncoder) to SecurityConfig

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/config/SecurityConfig.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/config/SecurityConfig.kt
@@ -7,6 +7,8 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
@@ -39,4 +41,7 @@ class SecurityConfig(
 
         return http.build()
     }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
