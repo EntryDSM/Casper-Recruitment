dc69b882a9020463d40a0a1297bb42aee1c12ccc
coehgns <modooboiroo@gmail.com>
2025-03-11 20:16:57 +0900

    refactor : (#32) TokenRefreshService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/TokenRefreshService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/TokenRefreshService.kt
@@ -1,31 +1,16 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.service
 
-import entry.dsm.gitauth.equusgithubauth.domain.auth.entity.RefreshToken
-import entry.dsm.gitauth.equusgithubauth.domain.auth.entity.repository.RefreshTokenRepository
-import entry.dsm.gitauth.equusgithubauth.domain.auth.exception.RefreshTokenNotFoundException
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.TokenResponse
-import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtProperties
 import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TokenRefreshService(
-    private val jwtTokenProvider: JwtTokenProvider,
-    private val refreshTokenRepository: RefreshTokenRepository,
-    private val jwtProperties: JwtProperties,
+    private val jwtTokenProvider: JwtTokenProvider
 ) {
     @Transactional
     fun execute(refreshToken: String): TokenResponse {
-        val redisRefreshToken: RefreshToken =
-            refreshTokenRepository.findByRefreshToken(refreshToken)
-                ?: throw RefreshTokenNotFoundException
-
-        val userName = redisRefreshToken.userName
-        val tokens = jwtTokenProvider.generateToken(userName)
-
-        redisRefreshToken.updateToken(tokens.refreshToken, jwtProperties.refreshExp)
-
-        return tokens
+        return jwtTokenProvider.reissue(refreshToken)
     }
 }
