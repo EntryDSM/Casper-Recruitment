2c93df53cca345ed791b341399cb56a6647ec124
도훈 <modooboiroo@gmail.com>
2025-03-12 09:38:24 +0900

    refactor : user related logic

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/LoginService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/LoginService.kt
@@ -3,7 +3,9 @@ package entry.dsm.gitauth.equusgithubauth.domain.auth.service
 import entry.dsm.gitauth.equusgithubauth.domain.auth.exception.PasswordMissMatchException
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request.LoginRequest
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.TokenResponse
+import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
 import entry.dsm.gitauth.equusgithubauth.domain.user.userFacade.UserFacade
+import entry.dsm.gitauth.equusgithubauth.global.security.auth.exception.UserNotFoundException
 import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -13,11 +15,12 @@ import org.springframework.transaction.annotation.Transactional
 class LoginService(
     private val passwordEncoder: PasswordEncoder,
     private val jwtTokenProvider: JwtTokenProvider,
-    private val userFacade: UserFacade,
+    private val userRepository: UserRepository
 ) {
     @Transactional
     fun execute(request: LoginRequest): TokenResponse {
-        val user = userFacade.findByUserName(request.userName)
+        val user = userRepository.findByUserName(request.userName)
+            ?: throw UserNotFoundException
 
         if (passwordEncoder.matches(user.password, request.password)) {
             throw PasswordMissMatchException
--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/userFacade/UserFacade.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/userFacade/UserFacade.kt
@@ -3,13 +3,15 @@ package entry.dsm.gitauth.equusgithubauth.domain.user.userFacade
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.User
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
 import entry.dsm.gitauth.equusgithubauth.global.security.auth.exception.UserNotFoundException
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 
 @Component
 class UserFacade(
     private val userRepository: UserRepository,
 ) {
-    fun findByUserName(userName: String): User {
+    fun currentUser(): User {
+        val userName = SecurityContextHolder.getContext().authentication.name
         return userRepository.findByUserName(userName)
             ?: throw UserNotFoundException
     }
