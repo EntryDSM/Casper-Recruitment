4fdd00ed2d80cf25aad4c0dfadb516462990cf28
박주원 <qkrwndnjs7308@gmail.com>
2025-03-18 19:17:40 +0900

    feat : (#36) add exception

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/security/auth/exception/RefreshTokenNotFoundException.kt
@@ -0,0 +1,7 @@
+package entry.dsm.gitauth.equusgithubauth.global.security.auth.exception
+
+import entry.dsm.gitauth.equusgithubauth.global.exception.CustomException
+import entry.dsm.gitauth.equusgithubauth.global.exception.ErrorCode
+
+class RefreshTokenNotFoundException : CustomException(ErrorCode.REFRESH_TOKEN_NOT_FOUND) {
+}
