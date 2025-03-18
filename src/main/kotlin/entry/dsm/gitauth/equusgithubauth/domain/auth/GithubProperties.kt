5998fe7ae02b02dff8302925ae3e376c997418f7
도훈 <modooboiroo@gmail.com>
2025-03-19 08:36:16 +0900

    feat : (#35) GithubProperties

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/GithubProperties.kt
@@ -0,0 +1,10 @@
+package entry.dsm.gitauth.equusgithubauth.domain.auth
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "spring.security.oauth2.client.registration.github")
+class GithubProperties(
+    val clientId: String,
+    val clientSecret: String,
+    val redirectUri: String
+)
