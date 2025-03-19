f71b2e357e164012578c22463ff96a439b9d3e54
도훈 <modooboiroo@gmail.com>
2025-03-19 09:15:24 +0900

    feat : (#35) add tokenUri field to GithubProperties

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/GithubProperties.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/GithubProperties.kt
@@ -6,5 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 class GithubProperties(
     val clientId: String,
     val clientSecret: String,
-    val redirectUri: String
+    val redirectUri: String,
+    val tokenUri: String
 )
