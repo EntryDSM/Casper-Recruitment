d4e4fb4627cb1d230c33e6fef086bcec0a727c10
도훈 <modooboiroo@gmail.com>
2025-03-20 19:18:31 +0900

    refactor : (#35) UserService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
@@ -1,41 +1,73 @@
 package entry.dsm.gitauth.equusgithubauth.domain.user.service
 
-import entry.dsm.gitauth.equusgithubauth.domain.auth.service.GithubUserService
+import entry.dsm.gitauth.equusgithubauth.domain.auth.exception.OrganizationMembershipErrorException
+import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.LoginSuccessResponse
+import entry.dsm.gitauth.equusgithubauth.domain.auth.service.CreateGithubTokenService
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.User
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
 import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response.TokenResponse
+import entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.controller.GithubApiClient
 import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
+import feign.FeignException
+import feign.Response
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
     private val userRepository: UserRepository,
-    private val githubUserService: GithubUserService,
     private val jwtTokenProvider: JwtTokenProvider,
+    private val githubApiClient: GithubApiClient,
 ) {
-    fun execute(oAuth2User: OAuth2User): TokenResponse {
-        val userInfo = githubUserService.getGithubUserInformation(oAuth2User)
+    fun execute(accessToken: String): LoginSuccessResponse {
+        val userInfo = githubApiClient.getUser("Bearer $accessToken")
+        println("userInfo login: ${userInfo.login}")
+        val tokens = jwtTokenProvider.generateToken(userInfo.login)
+        val isUser = validateUserOrganization("Bearer $accessToken", userInfo.login)
 
         val user =
             User(
-                githubId = userInfo.githubId,
-                username = userInfo.username,
+                githubId = userInfo.login,
+                username = userInfo.name,
                 email = userInfo.email,
-                profileUrl = userInfo.profileUrl,
+                profileUrl = userInfo.htmlUrl,
                 avatarUrl = userInfo.avatarUrl,
                 createdAt = userInfo.createdAt,
                 updatedAt = userInfo.updatedAt,
-                accessToken = userInfo.accessToken,
-                tokenExpiration = userInfo.tokenExpiration,
+                accessToken = tokens.accessToken,
+                tokenExpiration = tokens.accessTokenExpiration
             )
 
-        val token = jwtTokenProvider.generateToken(userInfo.githubId)
-
-        if (userRepository.findByGithubId(userInfo.githubId) != null) {
-            return token
+        if (userRepository.findByGithubId(userInfo.login) != null) {
+            return LoginSuccessResponse(
+                isUser,
+                tokens.accessToken,
+                tokens.accessTokenExpiration,
+                tokens.refreshToken,
+                tokens.refreshTokenExpiration
+            )
         }
+
         userRepository.save(user)
-        return token
+        return LoginSuccessResponse(
+            isUser,
+            tokens.accessToken,
+            tokens.accessTokenExpiration,
+            tokens.refreshToken,
+            tokens.refreshTokenExpiration
+        )
+    }
+
+    fun validateUserOrganization(accessToken: String, username: String): Boolean {
+        return try {
+            githubApiClient.checkOrganization(accessToken, username)
+            false
+        } catch (e: FeignException.NotFound) {
+            true
+        } catch (e: FeignException) {
+            throw OrganizationMembershipErrorException
+        }
     }
 }
