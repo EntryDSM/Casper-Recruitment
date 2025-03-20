30e6a96d4dd2ac1743dd3d815b27efe1ab4b7604
도훈 <modooboiroo@gmail.com>
2025-03-20 20:12:15 +0900

    refactor : (#35) CreateGithubTokenService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/CreateGithubTokenService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/CreateGithubTokenService.kt
@@ -1,6 +1,7 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.service
 
 import entry.dsm.gitauth.equusgithubauth.domain.auth.GithubProperties
+import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.GithubAccessTokenResponse
 import entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.controller.GithubOAuthApiClient
 import org.springframework.stereotype.Service
 
@@ -9,13 +10,13 @@ class CreateGithubTokenService(
     private val githubOAuthApiClient: GithubOAuthApiClient,
     private val githubProperties: GithubProperties
 ) {
-    fun execute(code: String): String {
+    fun execute(code: String): GithubAccessTokenResponse {
         val response = githubOAuthApiClient.codeToToken(
             redirectUri = githubProperties.redirectUri,
             clientSecret = githubProperties.clientSecret,
             clientId = githubProperties.clientId,
             code = code
         )
-        return response.accessToken
+        return GithubAccessTokenResponse(response.accessToken)
     }
 }
