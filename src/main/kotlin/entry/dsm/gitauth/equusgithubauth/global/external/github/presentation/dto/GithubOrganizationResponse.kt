7cc728dc67156af2e8421603ffbfdb8d9de658ba
도훈 <modooboiroo@gmail.com>
2025-03-20 19:19:13 +0900

    refactor : (#35) GithubOrganizationResponse

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/external/github/presentation/dto/GithubOrganizationResponse.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/external/github/presentation/dto/GithubOrganizationResponse.kt
@@ -1,5 +1,8 @@
 package entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class GithubOrganizationResponse(
     val login: String,
 )
