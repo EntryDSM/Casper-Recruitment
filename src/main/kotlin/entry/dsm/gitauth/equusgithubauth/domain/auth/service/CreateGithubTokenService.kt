3df723477ab23f2e2c902f87a40d71b0e1ea1775
도훈 <modooboiroo@gmail.com>
2025-03-20 19:18:58 +0900

    refactor : (#35) CreateGithubTokenService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/CreateGithubTokenService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/CreateGithubTokenService.kt
@@ -1,33 +1,21 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.service
 
 import entry.dsm.gitauth.equusgithubauth.domain.auth.GithubProperties
-import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request.GithubAccessTokenRequest
-import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.response.GithubAccessTokenResponse
+import entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.controller.GithubOAuthApiClient
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
 
 @Service
 class CreateGithubTokenService(
-    private val githubProperties: GithubProperties,
-    private val restTemplate: RestTemplate
+    private val githubOAuthApiClient: GithubOAuthApiClient,
+    private val githubProperties: GithubProperties
 ) {
-    companion object {
-        private const val ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token"
+    fun execute(code: String): String {
+        val response = githubOAuthApiClient.codeToToken(
+            redirectUri = githubProperties.redirectUri,
+            clientSecret = githubProperties.clientSecret,
+            clientId = githubProperties.clientId,
+            code = code
+        )
+        return response.accessToken
     }
-    fun execute(
-        code: String
-    ): GithubAccessTokenResponse {
-        return restTemplate.postForObject(
-            ACCESS_TOKEN_URL,
-            GithubAccessTokenRequest(
-                clientId = githubProperties.clientId,
-                clientSecret = githubProperties.clientSecret,
-                code = code,
-                redirectUri = githubProperties.redirectUri
-            ),
-            GithubAccessTokenResponse::class.java
-        )!!
-    }
-
-
 }
