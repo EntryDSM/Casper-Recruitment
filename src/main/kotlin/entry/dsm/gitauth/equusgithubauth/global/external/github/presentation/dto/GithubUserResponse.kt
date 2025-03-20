fc77142109defc9c9f2cf93972f7006635cf97c6
도훈 <modooboiroo@gmail.com>
2025-03-20 19:19:06 +0900

    refactor : (#35) GithubUserResponse

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/external/github/presentation/dto/GithubUserResponse.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/external/github/presentation/dto/GithubUserResponse.kt
@@ -1,5 +1,18 @@
 package entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.time.LocalDateTime
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(SnakeCaseStrategy::class)
 data class GithubUserResponse(
     val login: String,
+    val name: String,
+    val email: String?,
+    val htmlUrl: String,
+    val avatarUrl: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
 )
