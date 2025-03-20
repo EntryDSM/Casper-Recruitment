22dbb1e42ef3082c4f76862df34cfe9d7de54062
도훈 <modooboiroo@gmail.com>
2025-03-20 20:09:46 +0900

    chore : (#35) remove unnecessary class

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/exception/InvalidAccessTokenException.kt
+++
@@ -1,6 +0,0 @@
-package entry.dsm.gitauth.equusgithubauth.domain.auth.exception
-
-import entry.dsm.gitauth.equusgithubauth.global.exception.CustomException
-import entry.dsm.gitauth.equusgithubauth.global.exception.ErrorCode
-
-object InvalidAccessTokenException : CustomException(ErrorCode.INVALID_ACCESS_TOKEN)
