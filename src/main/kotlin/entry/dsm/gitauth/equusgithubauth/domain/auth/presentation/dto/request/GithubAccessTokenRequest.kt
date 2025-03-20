9dec4dd815f081a8d4e07af938dabd51a57307d6
도훈 <modooboiroo@gmail.com>
2025-03-21 08:30:57 +0900

    chore : (#35) remove GithubAccessTokenRequest.kt

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/dto/request/GithubAccessTokenRequest.kt
+++
@@ -1,8 +0,0 @@
-package entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.request
-
-data class GithubAccessTokenRequest(
-    val clientId: String,
-    val clientSecret: String,
-    val code: String,
-    val redirectUri: String,
-)
