9b2050274f87fb071c24445f56e5e635f9f474c5
도훈 <modooboiroo@gmail.com>
2025-03-20 23:45:43 +0900

    refactor : (#35) ValidateGithubOrganizationService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/ValidateGithubOrganizationService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/ValidateGithubOrganizationService.kt
@@ -1,22 +1,19 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.service
 
-import entry.dsm.gitauth.equusgithubauth.domain.auth.exception.OrganizationMembershipErrorException
 import entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.controller.GithubApiClient
-import feign.FeignException
 import org.springframework.stereotype.Service
 
 @Service
 class ValidateGithubOrganizationService(
     private val githubApiClient: GithubApiClient
 ) {
+    companion object {
+        private const val TARGET_ORG = "EntryDSM"
+    }
+
     fun execute(accessToken: String, username: String): Boolean {
-        return try {
-            githubApiClient.checkOrganization(accessToken, username)
-            false
-        } catch (e: FeignException.NotFound) {
-            true
-        } catch (e: FeignException) {
-            throw OrganizationMembershipErrorException
-        }
+        val userOrganizations = githubApiClient.getUserOrganizations(accessToken)
+
+        return userOrganizations.any { it.login == TARGET_ORG }
     }
 }
