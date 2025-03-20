126d7c84d5c9a020a7632a5c14125d6c49733fd7
도훈 <modooboiroo@gmail.com>
2025-03-20 19:19:27 +0900

    refactor : (#35) GithubAuthenticationController

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/GithubAuthenticationController.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/GithubAuthenticationController.kt
@@ -1,17 +1,20 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.presentation
 
-import entry.dsm.gitauth.equusgithubauth.domain.auth.command.service.CreateGithubTokenService
-import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response.TokenResponse
+import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.LoginSuccessResponse
+import entry.dsm.gitauth.equusgithubauth.domain.auth.service.CreateGithubTokenService
+import entry.dsm.gitauth.equusgithubauth.domain.user.service.UserService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.view.RedirectView
 
 @RestController
-@RequestMapping("/github")
+@RequestMapping("/api/github/auth")
 class GithubAuthenticationController(
-    val createGithubTokenService: CreateGithubTokenService
+    private val createGithubTokenService: CreateGithubTokenService,
+    private val userService: UserService
 ) {
     // redirect
     @GetMapping
@@ -20,10 +23,19 @@ class GithubAuthenticationController(
     }
 
     // code to token
-    @GetMapping("/callback")
-    fun generateGithubToken(
-        @RequestParam code: String
-    ): TokenResponse {
+    @GetMapping("/login/oauth2/code/github")
+    fun githubCallback(@RequestParam code: String): String {
+        println("code : $code")
         return createGithubTokenService.execute(code)
+    }
+
+    @GetMapping("/authenticated")
+    fun githubLoginSuccess(@RequestHeader("Authorization") accessToken: String): LoginSuccessResponse {
+        return userService.execute(accessToken)
+    }
+
+    @GetMapping("/not/authenticated")
+    fun githubLoginFailure(): String {
+        return "Not authenticated"
     }
 }
