0e47e0db67f86e7b8a2dd89109222d3d643848d3
도훈 <modooboiroo@gmail.com>
2025-03-20 23:48:27 +0900

    refactor : (#35) remove unnecessary import to UserService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
@@ -1,19 +1,11 @@
 package entry.dsm.gitauth.equusgithubauth.domain.user.service
 
-import entry.dsm.gitauth.equusgithubauth.domain.auth.exception.OrganizationMembershipErrorException
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.LoginSuccessResponse
-import entry.dsm.gitauth.equusgithubauth.domain.auth.service.CreateGithubTokenService
 import entry.dsm.gitauth.equusgithubauth.domain.auth.service.ValidateGithubOrganizationService
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.User
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
-import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response.TokenResponse
 import entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.controller.GithubApiClient
 import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
-import feign.FeignException
-import feign.Response
-import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.NOT_FOUND
-import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
 
 @Service
