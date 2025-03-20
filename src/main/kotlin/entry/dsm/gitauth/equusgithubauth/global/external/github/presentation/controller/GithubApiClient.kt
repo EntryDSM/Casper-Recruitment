eb520510d0130c72b92c413ff8fc96e2de83cef5
도훈 <modooboiroo@gmail.com>
2025-03-20 19:18:50 +0900

    refactor : (#35) GithubApiClient

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/external/github/presentation/controller/GithubApiClient.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/external/github/presentation/controller/GithubApiClient.kt
@@ -2,6 +2,7 @@ package entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.co
 
 import entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.dto.GithubOrganizationResponse
 import entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.dto.GithubUserResponse
+import feign.Response
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.RequestHeader
 @FeignClient(name = "githubApiClient", url = "https://api.github.com")
 interface GithubApiClient {
     @GetMapping("/user")
-    fun getUser(
-        @RequestHeader("Authorization") authorization: String,
-    ): GithubUserResponse
+    fun getUser(@RequestHeader("Authorization") authorization: String): GithubUserResponse
 
     @GetMapping("/user/orgs")
-    fun getUserOrganizations(
+    fun getUserOrganizations(@RequestHeader("Authorization") authorization: String): List<GithubOrganizationResponse>
+
+    @GetMapping("/orgs/EntryDSM/members/{username}")
+    fun checkOrganization(
         @RequestHeader("Authorization") authorization: String,
-    ): List<GithubOrganizationResponse>
+        @PathVariable username: String
+    ): Response
 }
