6f8969b1a42796fc12ddd52f1c280630f7d48dc3
도훈 <modooboiroo@gmail.com>
2025-03-21 16:11:08 +0900

    feat : (#35) add scope and clientName field to GithubRegistrationProperties

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/GithubRegistrationProperties.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/GithubRegistrationProperties.kt
@@ -3,8 +3,10 @@ package entry.dsm.gitauth.equusgithubauth.domain.auth
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "spring.security.oauth2.client.registration.github")
-class GithubProperties(
+class GithubRegistrationProperties(
     val clientId: String,
     val clientSecret: String,
     val redirectUri: String,
+    val scope: String,
+    val clientName: String
 )
