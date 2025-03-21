e8fc747a5e635d0dee98b43dd5c1fae2445c537b
도훈 <modooboiroo@gmail.com>
2025-03-21 16:10:43 +0900

    refactor : (#35) modify DI to GenerateGithubTokenService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/GenerateGithubTokenService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/service/GenerateGithubTokenService.kt
@@ -1,20 +1,20 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.service
 
-import entry.dsm.gitauth.equusgithubauth.domain.auth.GithubProperties
+import entry.dsm.gitauth.equusgithubauth.domain.auth.GithubRegistrationProperties
 import entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.dto.response.GithubAccessTokenResponse
 import entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.controller.GithubOAuthApiClient
 import org.springframework.stereotype.Service
 
 @Service
-class CreateGithubTokenService(
+class GenerateGithubTokenService(
     private val githubOAuthApiClient: GithubOAuthApiClient,
-    private val githubProperties: GithubProperties
+    private val githubRegistrationProperties: GithubRegistrationProperties
 ) {
     fun execute(code: String): GithubAccessTokenResponse {
         val response = githubOAuthApiClient.codeToToken(
-            redirectUri = githubProperties.redirectUri,
-            clientSecret = githubProperties.clientSecret,
-            clientId = githubProperties.clientId,
+            redirectUri = githubRegistrationProperties.redirectUri,
+            clientSecret = githubRegistrationProperties.clientSecret,
+            clientId = githubRegistrationProperties.clientId,
             code = code
         )
 
