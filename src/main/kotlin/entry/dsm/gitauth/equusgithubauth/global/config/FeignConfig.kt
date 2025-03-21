8a0a5448ab88e37ddc6b4f2d28f61f49ef33ad7e
도훈 <modooboiroo@gmail.com>
2025-03-21 16:11:39 +0900

    feat : (#35) add @EnableConfigurationProperties of attribute to FeignConfig

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/config/FeignConfig.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/config/FeignConfig.kt
@@ -1,12 +1,17 @@
 package entry.dsm.gitauth.equusgithubauth.global.config
 
-import entry.dsm.gitauth.equusgithubauth.domain.auth.GithubProperties
+import entry.dsm.gitauth.equusgithubauth.domain.auth.GithubRegistrationProperties
+import entry.dsm.gitauth.equusgithubauth.domain.auth.service.GithubProviderProperties
 import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Configuration
 
 @EnableFeignClients(basePackages = ["entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.controller"])
-@EnableConfigurationProperties(value = [ JwtProperties::class, GithubProperties::class ])
+@EnableConfigurationProperties(value = [
+    JwtProperties::class,
+    GithubRegistrationProperties::class,
+    GithubProviderProperties::class
+])
 @Configuration
 class FeignConfig
