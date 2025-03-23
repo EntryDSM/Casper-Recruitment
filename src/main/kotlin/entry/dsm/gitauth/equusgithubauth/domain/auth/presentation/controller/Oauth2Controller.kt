43f5c36ca2846062fb468d4ae25583aff9a5dfb3
박주원 <qkrwndnjs7308@gmail.com>
2025-03-23 16:13:33 +0900

    refactor : (#42) modify redirect

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/controller/Oauth2Controller.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/controller/Oauth2Controller.kt
@@ -1,34 +1,25 @@
 package entry.dsm.gitauth.equusgithubauth.domain.auth.presentation.controller
 
-
 import entry.dsm.gitauth.equusgithubauth.domain.auth.service.LogoutService
 import entry.dsm.gitauth.equusgithubauth.domain.auth.service.OauthReissueService
 import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.request.LogoutRequest
 import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.request.RefreshTokenRequest
 import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response.TokenResponse
-import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.view.RedirectView
 
 @RestController
 @RequestMapping("/api/google/auth")
 class Oauth2Controller(
-    @Value("\${oauth.google.redirect") private val googleRedirectUrl : String,
+    @Value("\${oauth.google.redirect}") private val googleRedirectUrl: String,
     private val logoutService: LogoutService,
     private val oauthReissueService: OauthReissueService
-
 ) {
 
     @GetMapping
     fun googleAuth(): RedirectView {
-        return RedirectView("/oauth2/authorization/google")
+        return RedirectView(googleRedirectUrl)
     }
 
     @PatchMapping("/reissue")
@@ -40,7 +31,4 @@ class Oauth2Controller(
     fun logout(@RequestBody request: LogoutRequest) {
         logoutService.logout(request.accessToken)
     }
-
-
-
 }
