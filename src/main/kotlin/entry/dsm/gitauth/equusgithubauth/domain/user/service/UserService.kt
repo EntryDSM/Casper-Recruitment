bf1a8c5e35ec5ce1029e9cc9d35f4c3cf5f5352e
박주원 <qkrwndnjs7308@gmail.com>
2025-03-23 16:16:51 +0900

    refactor : (#42) add roleProvider

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
@@ -6,6 +6,7 @@ import entry.dsm.gitauth.equusgithubauth.domain.user.entity.enums.UserRole
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
 import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response.LoginSuccessResponse
 import entry.dsm.gitauth.equusgithubauth.global.external.github.presentation.controller.GithubApiClient
+import entry.dsm.gitauth.equusgithubauth.global.oauth.service.component.OauthRoleProvider
 import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
 import org.springframework.stereotype.Service
 
@@ -15,18 +16,21 @@ class UserService(
     private val jwtTokenProvider: JwtTokenProvider,
     private val githubApiClient: GithubApiClient,
     private val validateGithubOrganizationService: ValidateGithubOrganizationService,
+    private val oauthRoleProvider: OauthRoleProvider
 ) {
     fun execute(accessToken: String): LoginSuccessResponse {
         val userInfo = githubApiClient.getUser("Bearer $accessToken")
         val tokens = jwtTokenProvider.generateToken(userInfo.login)
         val isMember = validateGithubOrganizationService.execute("Bearer $accessToken")
 
+        val provider = "GITHUB" // 지금 GitHub OAuth 로직에는 provider가 없으므로 하드코딩
+
         val user =
             User(
                 loginId = userInfo.login,
                 name = userInfo.name,
                 email = userInfo.email,
-                role = UserRole.ADMIN
+                role = oauthRoleProvider.getRoleByProvider(provider)
             )
 
         userRepository.save(user)
