4577f158d3b3f8bd1bdb278f5f14e4a2514056ab
박주원 <qkrwndnjs7308@gmail.com>
2025-03-23 15:35:19 +0900

    feat : (#42) add emailDomainValidator

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/oauth/service/component/EmailDomainValidator.kt
@@ -0,0 +1,18 @@
+package entry.dsm.gitauth.equusgithubauth.global.oauth.service.component
+
+import entry.dsm.gitauth.equusgithubauth.global.oauth.exception.EmailIsNotAllowed
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class EmailDomainValidator(
+    @Value("\${email.allowed") private val allowedDomain : String,
+) {
+
+
+    fun isAllowed(email: String) {
+        if (!email.endsWith("@$allowedDomain")) {
+            throw EmailIsNotAllowed()
+        }
+    }
+}
