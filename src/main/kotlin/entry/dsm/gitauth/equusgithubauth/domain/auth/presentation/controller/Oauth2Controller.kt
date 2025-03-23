c8bef0097eed267d99f7450e3fa63bd0c471606a
박주원 <qkrwndnjs7308@gmail.com>
2025-03-23 15:34:05 +0900

    refactor : (#42) modify redirect

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/controller/Oauth2Controller.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/controller/Oauth2Controller.kt
@@ -10,27 +10,28 @@ import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
 
 @RestController
 @RequestMapping("/api/google/auth")
 class Oauth2Controller(
     @Value("\${oauth.google.redirect") private val googleRedirectUrl : String,
-    private val jwtTokenProvider: JwtTokenProvider,
     private val logoutService: LogoutService,
     private val oauthReissueService: OauthReissueService
 
 ) {
 
     @GetMapping
-    fun googleAuth(): String {
-        return "redirect:$googleRedirectUrl"
+    fun googleAuth(): RedirectView {
+        return RedirectView("/oauth2/authorization/google")
     }
 
-    @PostMapping("/reissue")
+    @PatchMapping("/reissue")
     fun reissueToken(@RequestBody request: RefreshTokenRequest): TokenResponse {
         return oauthReissueService.reissue(request.refreshToken)
     }
