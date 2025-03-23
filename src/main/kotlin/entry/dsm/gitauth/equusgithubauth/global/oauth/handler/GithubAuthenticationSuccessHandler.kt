41196d4bb21196380bd8721cd19e1610e3348afc
도훈 <modooboiroo@gmail.com>
2025-03-23 23:04:20 +0900

    refactor: (#35) using githubAuthProperties to GithubAuthenticationSuccessHandler

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/oauth/handler/GithubAuthenticationSuccessHandler.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/oauth/handler/GithubAuthenticationSuccessHandler.kt
@@ -1,16 +1,19 @@
 package entry.dsm.gitauth.equusgithubauth.global.oauth.handler
 
+import entry.dsm.gitauth.equusgithubauth.domain.auth.GithubAuthProperties
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 
-class GithubAuthenticationSuccessHandler : AuthenticationSuccessHandler {
+class GithubAuthenticationSuccessHandler(
+    private val githubAuthProperties: GithubAuthProperties
+) : AuthenticationSuccessHandler {
     override fun onAuthenticationSuccess(
         request: HttpServletRequest,
         response: HttpServletResponse,
         authentication: Authentication,
     ) {
-        response.sendRedirect("/api/github/auth/authenticated/")
+        response.sendRedirect(githubAuthProperties.successAuthenticationUrl)
     }
 }
