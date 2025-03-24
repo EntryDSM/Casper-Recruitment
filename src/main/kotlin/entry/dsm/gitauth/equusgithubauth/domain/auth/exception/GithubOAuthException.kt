c032b5bc59f0f1739d558ccc31ea25b4fab260a8
도훈 <modooboiroo@gmail.com>
2025-03-24 20:12:03 +0900

    refactor : (#35) modify GithubOauthException naming

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/exception/GithubOAuthException.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/exception/GithubOAuthException.kt
@@ -3,4 +3,4 @@ package entry.dsm.gitauth.equusgithubauth.domain.auth.exception
 import entry.dsm.gitauth.equusgithubauth.global.exception.CustomException
 import entry.dsm.gitauth.equusgithubauth.global.exception.ErrorCode
 
-class GithubOauthException : CustomException(ErrorCode.GITHUB_OAUTH)
+class GithubOAuthException : CustomException(ErrorCode.GITHUB_OAUTH)
