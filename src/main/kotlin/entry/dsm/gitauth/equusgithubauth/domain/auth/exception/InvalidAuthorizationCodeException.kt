04dc5e8379454776076a2b66de1b3a4364b7f0c9
도훈 <modooboiroo@gmail.com>
2025-03-24 09:00:20 +0900

    refactor: (#35) modify file form to InvalidAuthorizationCodeException

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/exception/InvalidAuthorizationCodeException.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/exception/InvalidAuthorizationCodeException.kt
@@ -3,4 +3,4 @@ package entry.dsm.gitauth.equusgithubauth.domain.auth.exception
 import entry.dsm.gitauth.equusgithubauth.global.exception.CustomException
 import entry.dsm.gitauth.equusgithubauth.global.exception.ErrorCode
 
-object InvalidAuthorizationCodeException : CustomException(ErrorCode.INVALID_AUTHORIZATION_CODE)
+class InvalidAuthorizationCodeException : CustomException(ErrorCode.INVALID_AUTHORIZATION_CODE)
