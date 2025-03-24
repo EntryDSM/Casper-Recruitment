bbbf64b0b7f7e0e7e0b830fb72a56894af3c3a24
도훈 <modooboiroo@gmail.com>
2025-03-24 19:48:39 +0900

    refactor : (#35) modify redirect-uri naming to GithubRegistrationProperties

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/GithubRegistrationProperties.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/GithubRegistrationProperties.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 class GithubRegistrationProperties(
     val clientId: String,
     val clientSecret: String,
-    val redirectUri: String,
+    val redirectUrl: String,
     val scope: String,
     val clientName: String,
 )
