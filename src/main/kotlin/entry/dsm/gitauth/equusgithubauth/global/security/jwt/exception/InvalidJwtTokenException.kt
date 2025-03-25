1290a1ff9fc39cc9ee85c2e29251d76f6699e052
박주원 <qkrwndnjs7308@gmail.com>
2025-03-25 19:01:23 +0900

    fix : (#42) solve the conflict

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/security/jwt/exception/InvalidJwtTokenException.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/security/jwt/exception/InvalidJwtTokenException.kt
@@ -3,8 +3,5 @@ package entry.dsm.gitauth.equusgithubauth.global.security.jwt.exception
 import entry.dsm.gitauth.equusgithubauth.global.exception.CustomException
 import entry.dsm.gitauth.equusgithubauth.global.exception.ErrorCode
 
-<<<<<<< HEAD:src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/security/jwt/exception/JwtTokenInvalidException.kt
+
 class JwtTokenInvalidException : CustomException(ErrorCode.JWT_TOKEN_INVALID)
-=======
-class InvalidJwtTokenException : CustomException(ErrorCode.JWT_TOKEN_INVALID)
->>>>>>> origin/feature/#35-github-oauth:src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/security/jwt/exception/InvalidJwtTokenException.kt
