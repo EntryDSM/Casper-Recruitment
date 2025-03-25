4b3112d74f38e4cf977bbcac2d3e14be94369cee
박주원 <qkrwndnjs7308@gmail.com>
2025-03-25 22:51:25 +0900

    refactor : (#44) modify buildSrc

--- buildSrc/src/main/kotlin/PluginVersions.kt
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -1,6 +1,6 @@
 object PluginVersions {
-    const val kotlin = "1.9.25"
-    const val springBoot = "3.4.0"
+    const val kotlin = "1.9.25" // 팀원들과 상의해서 정한 것
+    const val springBoot = "3.4.0" // 바꿀 수 없음
     const val dependencyManagement = "1.1.6"
     const val ktlint = "12.1.1"
 }
