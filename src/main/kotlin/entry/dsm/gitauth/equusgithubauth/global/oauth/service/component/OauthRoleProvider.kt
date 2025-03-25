362856e4567e34dd10eaefb156911eebb0125719
박주원 <qkrwndnjs7308@gmail.com>
2025-03-25 23:20:04 +0900

    refactor : (#42) remove else

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/oauth/service/component/OauthRoleProvider.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/oauth/service/component/OauthRoleProvider.kt
@@ -9,10 +9,6 @@ import org.springframework.stereotype.Component
 class OauthRoleProvider {
 
     fun getRoleByProvider(provider: String): UserRole {
-        return when (provider) {
-            OauthType.GITHUB.provider -> UserRole.ADMIN
-            OauthType.GOOGLE.provider -> UserRole.USER
-            else -> throw NotFoundOauthProvider()
-        }
+        return if (provider == OauthType.GITHUB.provider) UserRole.ADMIN else UserRole.USER
     }
 }
