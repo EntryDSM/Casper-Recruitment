80b5bd0a0609422a12642a1163e3f5affd5dc087
박주원 <qkrwndnjs7308@gmail.com>
2025-03-28 17:10:22 +0900

    refactor : (#52) ktlintFormat

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/security/jwt/JwtProperties.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "jwt")
 class JwtProperties(
-    val header: String ="Authorization",
+    val header: String,
     val prefix: String,
     val secretKey: String,
     val accessExp: Long,
