9f6df1db42e5fe52dd96de82bff5e092b59fccfe
박주원 <qkrwndnjs7308@gmail.com>
2025-03-28 22:13:04 +0900

    feat : (#52) add customErrorController

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/exception/CustomErrorController.kt
@@ -0,0 +1,28 @@
+package entry.dsm.gitauth.equusgithubauth.global.exception
+
+import org.springframework.boot.web.servlet.error.ErrorController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import jakarta.servlet.http.HttpServletRequest
+
+@RestController
+class CustomErrorController : ErrorController {
+
+    @RequestMapping("/error")
+    fun handleError(request: HttpServletRequest): ResponseEntity<Map<String, Any>> {
+        val status = request.getAttribute("javax.servlet.error.status_code") as? Int
+            ?: HttpStatus.INTERNAL_SERVER_ERROR.value()
+        val message = request.getAttribute("javax.servlet.error.message")?.toString()
+            ?: "알 수 없는 오류가 발생했습니다."
+
+        val errorResponse = mapOf(
+            "error" to "서버 오류",
+            "message" to message,
+            "status" to status
+        )
+
+        return ResponseEntity(errorResponse, HttpStatus.valueOf(status))
+    }
+}
