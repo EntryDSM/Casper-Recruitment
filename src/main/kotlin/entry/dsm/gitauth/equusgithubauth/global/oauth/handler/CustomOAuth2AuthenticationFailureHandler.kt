33a40544f2c54b6a8e50fc6116f3fccaefbd15e5
박주원 <qkrwndnjs7308@gmail.com>
2025-03-28 22:13:32 +0900

    refactor : (#52) modify failureHandler

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/oauth/handler/CustomOAuth2AuthenticationFailureHandler.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/oauth/handler/CustomOAuth2AuthenticationFailureHandler.kt
@@ -1,20 +1,30 @@
 package entry.dsm.gitauth.equusgithubauth.global.oauth.handler
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.authentication.AuthenticationFailureHandler
+import org.springframework.stereotype.Component
 
+@Component
 class CustomOAuth2AuthenticationFailureHandler : AuthenticationFailureHandler {
+
+    private val objectMapper = ObjectMapper()
+
     override fun onAuthenticationFailure(
         request: HttpServletRequest,
         response: HttpServletResponse,
-        exception: AuthenticationException,
+        exception: AuthenticationException
     ) {
-        response.contentType = "application/json"
         response.status = HttpServletResponse.SC_UNAUTHORIZED
-        response.writer.write(
-            """{error: OAuth2 로그인 실패, message: ${exception.message}}""",
+        response.contentType = "application/json; charset=UTF-8"
+
+        val errorResponse = mapOf(
+            "error" to "구글 로그인 실패",
+            "message" to (exception.message ?: "올바른 이메일 도메인이 아닙니다..")
         )
+
+        response.writer.write(objectMapper.writeValueAsString(errorResponse))
     }
 }
