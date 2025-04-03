729e2c20c9d0876af7f43711408b28c88f32c0b8
박주원 <qkrwndnjs7308@gmail.com>
2025-04-03 10:30:14 +0900

    refactor : (#57) add tranctional

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/query/service/GetAllNoticesService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/notice/query/service/GetAllNoticesService.kt
@@ -3,10 +3,12 @@ package entry.dsm.gitauth.equusgithubauth.domain.notice.query.service
 import entry.dsm.gitauth.equusgithubauth.domain.notice.query.dto.response.NoticeAllQueryResponse
 import entry.dsm.gitauth.equusgithubauth.domain.notice.query.repository.NoticeQueryRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class GetAllNoticesService(
     private val noticeQueryRepository: NoticeQueryRepository,
 ) {
+    @Transactional(readOnly = true)
     fun getAllNotices(): List<NoticeAllQueryResponse> = noticeQueryRepository.findAll().map(NoticeAllQueryResponse::from)
 }
