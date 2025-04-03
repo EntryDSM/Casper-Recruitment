e35113f83d2b6303ee6b9077ddeb7c12e759caa9
박주원 <qkrwndnjs7308@gmail.com>
2025-04-03 10:22:35 +0900

    feat : (#57) add baseUUIDEntity

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/entity/BaseTimeEntity.kt
@@ -0,0 +1,19 @@
+package entry.dsm.gitauth.equusgithubauth.global.entity
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseTimeEntity {
+
+    @CreatedDate
+    val createAt: LocalDateTime = LocalDateTime.now()
+    @LastModifiedDate
+    val modifiedAt: LocalDateTime = LocalDateTime.now()
+}
---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/global/entity/BaseUUIDEntity.kt
@@ -0,0 +1,20 @@
+package entry.dsm.gitauth.equusgithubauth.global.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.UuidGenerator
+import org.hibernate.annotations.UuidGenerator.Style
+import java.util.UUID
+@MappedSuperclass
+abstract class BaseUUIDEntity(
+    id: UUID?
+) {
+    @Id
+    @UuidGenerator(style = Style.RANDOM)
+    @GeneratedValue
+    @Column(columnDefinition = "BINARY(16)", nullable = false)
+    val id: UUID? = if (id == UUID(0,0)) null else id
+
+}
