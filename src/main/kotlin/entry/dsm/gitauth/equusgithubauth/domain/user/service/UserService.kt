8505c2946ebcdf5f8fce669c02b97bb191a2499d
박주원 <qkrwndnjs7308@gmail.com>
2025-04-07 23:50:06 +0900

    refactor : (#68) modify userService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/user/service/UserService.kt
@@ -6,6 +6,7 @@ import entry.dsm.gitauth.equusgithubauth.domain.user.entity.User
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.enums.UserRole
 import entry.dsm.gitauth.equusgithubauth.domain.user.entity.repository.UserRepository
 import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response.TokenResponse
+import entry.dsm.gitauth.equusgithubauth.domain.user.service.component.UserSaver
 import entry.dsm.gitauth.equusgithubauth.global.infrastructure.feign.client.GithubApiClient
 import entry.dsm.gitauth.equusgithubauth.global.security.jwt.JwtTokenProvider
 import org.springframework.stereotype.Service
@@ -13,16 +14,19 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserService(
-    private val userRepository: UserRepository,
+    private val userSaver: UserSaver,
     private val jwtTokenProvider: JwtTokenProvider,
     private val githubApiClient: GithubApiClient,
     private val validateGithubOrganizationService: ValidateGithubOrganizationService,
 ) {
     companion object {
         private fun withBearer(accessToken: String) = "Bearer $accessToken"
+
+        private val userSaverName = "UNKNOWN"
+        private val userSaverEmail = "no-email@unknown.com"
     }
 
-    @Transactional
+
     fun generateTokens(accessToken: String): TokenResponse {
         val bearerToken = withBearer(accessToken)
         val userInfo = githubApiClient.getUser(bearerToken)
@@ -32,19 +36,11 @@ class UserService(
             throw UnAuthorizedOrgAccessException()
         }
 
-        val existUser = userRepository.findByLoginId(userInfo.login)
-
-        if (existUser == null) {
-            val user =
-                User(
-                    loginId = userInfo.login,
-                    name = userInfo.name,
-                    email = userInfo.email,
-                    role = UserRole.ADMIN,
-                )
-
-            userRepository.save(user)
-        }
+        userSaver.findOrCreateUser(
+            loginId = userInfo.login,
+            name = userInfo.name ?: userSaverName,
+            email = userInfo.email ?: userSaverEmail,
+        )
 
         return TokenResponse(
             tokens.accessToken,
@@ -54,3 +50,4 @@ class UserService(
         )
     }
 }
+
