e07f39d472cbd43496e7011c1d0a5dad4db00924
도훈 <modooboiroo@gmail.com>
2025-04-07 22:15:20 +0900

    refactor : (#68) add @Transactional to GitHubOauthService

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/command/service/GitHubOauthService.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/command/service/GitHubOauthService.kt
@@ -6,6 +6,7 @@ import entry.dsm.gitauth.equusgithubauth.domain.user.service.UserService
 import entry.dsm.gitauth.equusgithubauth.global.infrastructure.feign.client.GithubOAuthApiClient
 import entry.dsm.gitauth.equusgithubauth.global.oauth.properties.GithubRegistrationProperties
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class GitHubOauthService(
@@ -13,6 +14,7 @@ class GitHubOauthService(
     private val githubRegistrationProperties: GithubRegistrationProperties,
     private val userService: UserService,
 ) {
+    @Transactional
     fun execute(code: String): TokenResponse {
         try {
             val githubAccessToken =
