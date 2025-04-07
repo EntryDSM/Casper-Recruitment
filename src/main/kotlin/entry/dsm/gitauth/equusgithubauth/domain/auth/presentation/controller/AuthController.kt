5c7c2d27426c38fd5b3e7027d0546b4d9331d52f
박주원 <qkrwndnjs7308@gmail.com>
2025-04-07 22:49:36 +0900

    chore : (#68) add authController

--- src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/controller/AuthController.kt
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/domain/auth/presentation/controller/AuthController.kt
@@ -5,37 +5,26 @@ import entry.dsm.gitauth.equusgithubauth.domain.auth.command.service.OauthReissu
 import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.request.LogoutRequest
 import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.request.RefreshTokenRequest
 import entry.dsm.gitauth.equusgithubauth.domain.user.presentation.dto.response.TokenResponse
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.view.RedirectView
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/google/auth")
-class Oauth2Controller(
-    @Value("\${oauth.google.redirect}") private val googleRedirectUrl: String,
+@RequestMapping("/auth")
+class AuthController(
     private val logoutService: LogoutService,
     private val oauthReissueService: OauthReissueService,
-) {
-    @GetMapping
-    fun googleAuth(): RedirectView {
-        return RedirectView(googleRedirectUrl)
-    }
 
+    ) {
     @PatchMapping("/reissue")
     fun reissueToken(
-        @RequestBody request: RefreshTokenRequest,
+        @RequestBody @Valid request: RefreshTokenRequest,
     ): TokenResponse {
         return oauthReissueService.reissue(request.refreshToken)
     }
 
     @PostMapping("/logout")
     fun logout(
-        @RequestBody request: LogoutRequest,
+        @RequestBody @Valid request: LogoutRequest,
     ) {
         logoutService.logout(request.accessToken)
     }
